7a7873d2b6ab4f552595a5fec508d0605d47bae3
leandrop22 <leandropetricca123@gmail.com>
2025-07-21 11:11:56 -0300

    dale pue

--- tentcowork-frontend/vite.config.ts
+++ tentcowork-frontend/vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegura que las rutas de los assets se resuelvan desde la raíz
+  build: {
+    outDir: 'dist',
+  }
 });
